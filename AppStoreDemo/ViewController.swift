41d62c9023f82bfaa0afa33207b6c8b105616dda
FlyKite <DogeFlyKite@gmail.com>
2019-09-26 18:09:26 +0800

    Initial commit

---
+++ AppStoreDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreDemo
+//
+//  Created by FlyKite on 2019/9/26.
+//  Copyright © 2019 Doge Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
